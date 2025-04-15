3bb2ba521c696827cd82d5ee4688828d7fa9bf3f
Yash Amin <yashamin2007@gmail.com>
2025-04-15 16:18:39 -0400

    Mcq's Correction

--- backend/TaskManagerApi/obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
+++ backend/TaskManagerApi/obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84cb1d7f96387180628016954937a9574f3d7244")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
